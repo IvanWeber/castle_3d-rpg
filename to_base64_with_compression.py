b24f854f42d9d03f854a351d4d35cfba669394a0
Ivan <ivanbdn@yandex.ru>
2025-05-08 00:49:52 +0300

    add walls texture, but theres bug with smeared texture on sides

--- to_base64_with_compression.py
+++ to_base64_with_compression.py
@@ -26,4 +26,4 @@ def compress_and_encode_image(input_path, output_txt_path, scale_factor=0.33, qu
             out_file.write("data:image/jpeg;base64," + encoded)
 
 # Пример использования
-compress_and_encode_image("grass.png", "grass.txt")
+compress_and_encode_image("wall.png", "wall.txt")
